3e70e9be0ef1d599192efae6b4a464a53a99371e
Flávio Santos <flavio.santos@pravaler.com.br>
2023-04-25 21:00:20 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobolStyle } from "./styles/global"
+import { Transactions } from "./pages/Transactions"
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobolStyle />
-      <h1>APP - start new project</h1>
+        <Transactions />
     </ThemeProvider>
   )
 }
