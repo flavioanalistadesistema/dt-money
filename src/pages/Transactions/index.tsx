6e0ef104f50735cabffb4e447205e8e43bb3f38c
Flávio Santos <flavio.santos@pravaler.com.br>
2023-04-26 23:18:02 -0300

    tabela de transação

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceRighlight, TransactionTable, TransactionsContainer } from "./styles";
 
 export function Transactions() {
     return (
-        <>
+        <div>
             <Header />
             <Summary />
-        </>
+
+            <TransactionsContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceRighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceRighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceRighlight variant="oncome">
+                                    - 59,00
+                                </PriceRighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/04/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>
+            </TransactionsContainer>
+        </div>
     )
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const TransactionsContainer = styled.main`
+    width: 100%;
+    max-width: 70rem;
+    margin: 4rem auto 0;
+    padding: 0 1.5rem;
+`
+
+export const TransactionTable = styled.table`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+
+    td {
+        padding: 1.25rem 2rem;
+        background: ${props => props.theme["gray-700"]};
+
+        &:first-child {
+            border-top-left-radius: .375rem;
+            border-bottom-left-radius: .375rem;
+        }
+
+        &:last-child {
+            border-top-right-radius: .375rem;
+            border-bottom-right-radius: .375rem;
+        }
+    }
+`
+
+interface PriceRighlightProps {
+    variant?: 'income'| 'oncome'
+}
+
+export const PriceRighlight = styled.span<PriceRighlightProps>`
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]}
+`
