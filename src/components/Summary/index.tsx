fd25c7441e3062c35327fd4089e84bd20fbc836e
Flávio Santos <flavioanalistadesistema@gmail.com>
2023-05-12 10:48:59 -0300

    Calculando resumo de transações

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,28 @@
+import { useContext } from "react";
 import { SummaryCard, SummaryContainer } from "./styles";
 import { ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-export function Summary() {
+export function Summary() { 
+    const { transactions } = useContext(TransactionsContext)
+
+
+    // {income: 0, outcome: 0, total: 0}
+
+    const sumary = transactions.reduce(
+            (acc, transaction) => {
+
+                if (transaction.type === 'income') {
+                    acc.income += transaction.price
+                    acc.total += transaction.price
+                } else {
+                    acc.outcome += transaction.price
+                    acc.total -= transaction.price
+                }
+
+                return acc
+            }, {income: 0, outcome: 0, total: 0})
+
     return (
         <SummaryContainer>
             <SummaryCard>
@@ -9,21 +30,21 @@ export function Summary() {
                     <span>Entradas</span>
                     <ArrowCircleUp size={32} color="#00B37E"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.income}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Saidas</span>
                     <ArrowCircleUp size={32} color="#F75A68"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.outcome}</strong>
             </SummaryCard>
             <SummaryCard variant="green">
                 <header>
                     <span>Total</span>
                     <CurrencyDollar size={32} color="#ffffff"/>
                 </header>
-                <strong>R$ 17.400,00</strong>
+                <strong>{sumary.total}</strong>
             </SummaryCard>
         </SummaryContainer>
     )
